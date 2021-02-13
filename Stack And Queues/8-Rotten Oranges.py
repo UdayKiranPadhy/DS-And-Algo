928ba10a6f4050e2667ed55cb238233747145ed8
Uday Kiran <kiranu941@gmail.com>
2021-02-13 06:09:15 +0000

    gg

--- Stack And Queues/8-Rotten Oranges.py
+++ Stack And Queues/8-Rotten Oranges.py
@@ -59,76 +59,73 @@ At 2nd time frame:
 The 1 at the bottom left corner of the matrix is never rotten.
 
 """
+
+
 def issafe(i, j):
- 
-    if (i >= 0 and i < R and
-        j >= 0 and j < C):
+
+    if i >= 0 and i < R and j >= 0 and j < C:
         return True
     return False
- 
+
+
 def rotOranges(v):
- 
+
     changed = False
     no = 2
-    while (True):
+    while True:
         for i in range(R):
             for j in range(C):
- 
+
                 # Rot all other oranges
-                # present at (i+1, j), 
-                # (i, j-1), (i, j+1), 
+                # present at (i+1, j),
+                # (i, j-1), (i, j+1),
                 # (i-1, j)
-                if (v[i][j] == no):
-                    if (issafe(i + 1, j) and
-                        v[i + 1][j] == 1):
+                if v[i][j] == no:
+                    if issafe(i + 1, j) and v[i + 1][j] == 1:
                         v[i + 1][j] = v[i][j] + 1
                         changed = True
- 
-                    if (issafe(i, j + 1) and
-                        v[i][j + 1] == 1):
+
+                    if issafe(i, j + 1) and v[i][j + 1] == 1:
                         v[i][j + 1] = v[i][j] + 1
                         changed = True
- 
-                    if (issafe(i - 1, j) and
-                        v[i - 1][j] == 1):
+
+                    if issafe(i - 1, j) and v[i - 1][j] == 1:
                         v[i - 1][j] = v[i][j] + 1
                         changed = True
- 
-                    if (issafe(i, j - 1) and
-                        v[i][j - 1] == 1):
+
+                    if issafe(i, j - 1) and v[i][j - 1] == 1:
                         v[i][j - 1] = v[i][j] + 1
                         changed = True
- 
-        # if no rotten orange found 
+
+        # if no rotten orange found
         # it means all oranges rottened
         # now
-        if (not changed):
-           break
+        if not changed:
+            break
         changed = False
         no += 1
- 
+
     for i in range(R):
         for j in range(C):
- 
+
             # if any orange is found
-            # to be not rotten then 
+            # to be not rotten then
             # ans is not possible
-            if (v[i][j] == 1):
+            if v[i][j] == 1:
                 return -1
- 
+
     # Because initial value
     # for a rotten orange was 2
     return no - 2
- 
+
+
 # Driver function
 if __name__ == "__main__":
- 
-    v = [[2, 1, 0, 2, 1],
-         [1, 0, 1, 2, 1],
-         [1, 0, 0, 2, 1]]
-    R=len(v)
-    C=len(v[0])
- 
+
+    v = [[2, 1, 0, 2, 1], [1, 0, 1, 2, 1], [1, 0, 0, 2, 1]]
+    R = len(v)
+    C = len(v[0])
+
     # print("Max time incurred: ",
     #        rotOranges(v))
 
@@ -151,21 +148,69 @@ if the  distance is infinite return -1 .
 # Algorithm
 # For this we use BFS Approach of graphs to find the minimum distance.
 class QueueElement:
-    def __init__(self,i,j,T):
-        self.i=i
-        self.j=j
-        self.T=T
+    def __init__(self, i, j, T):
+        self.i = i
+        self.j = j
+        self.T = T
+
+
+def issafe(i, j, v):
+    if i >= 0 and i < len(v) and j >= 0 and j < len(v[0]):
+        return True
+    return False
+
 
 def RottonOranges(v):
-    list1=[]
+    list1 = []
+    time = 0
     # First append rotton oranges  indexes into list
     for i in range(len(v)):
         for j in range(len(v[0])):
-            if v[i][j]==2:
-                list1.append(QueueElement(i,j,0))
-    print(list1)
+            if v[i][j] == 2:
+                list1.append(QueueElement(i, j, time))
+    # Adding Delimiter
+    list1.append(QueueElement(-1, -1, time))
+    while len(list1) != 1:
+        time += 1
+        while list1[0].i != -1 and list1[0].j != -1:
+            i = list1[0].i
+            j = list1[0].j
 
-v = [[2, 1, 0, 2, 1],
-     [1, 0, 1, 2, 1],
-     [1, 0, 0, 2, 1]]
-RottonOranges(v)
+            if issafe(i + 1, j, v) and v[i + 1][j] == 1:
+                v[i + 1][j] = 2
+                list1.append(QueueElement(i + 1, j, time))
+
+            if issafe(i - 1, j, v) and v[i - 1][j] == 1:
+                v[i - 1][j] = 2
+                list1.append(QueueElement(i - 1, j, time))
+
+            if issafe(i, j + 1, v) and v[i][j + 1] == 1:
+                v[i][j + 1] = 2
+                list1.append(QueueElement(i, j + 1, time))
+
+            if issafe(i, j - 1, v) and v[i][j - 1] == 1:
+                v[i][j - 1] = 2
+                list1.append(QueueElement(i, j - 1, time))
+            del list1[0]
+        del list1[0]
+        list1.append(QueueElement(-1, -1, time))
+
+    for i in range(len(v)):
+        for j in range(len(v[0])):
+            if v[i][j] == 1:
+                return -1
+    return time - 1
+
+
+v = [[2, 1, 0, 2, 1], [1, 0, 1, 2, 1], [1, 0, 0, 2, 1]]
+print(RottonOranges(v))
+print(v)
+
+
+"""
+Complexity Analysis: 
+Time Complexity: O( R *C). 
+Each element of the matrix can be inserted into the queue only once so the upper bound of iteration is O(R*C), i.e. the number of elements. So time complexity is O(R *C).
+Space Complexity: O(R*C). 
+To store the elements in a queue O(R*C) space is needed.
+"""
