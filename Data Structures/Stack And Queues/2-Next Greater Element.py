9a39c20cc99c94bf4b46df4196d69badb7868b92
Uday Kiran <kiranu941@gmail.com>
2021-07-27 09:18:49 +0530

    gg

--- Data Structures/Stack And Queues/2-Next Greater Element.py
+++ Data Structures/Stack And Queues/2-Next Greater Element.py
@@ -1,9 +1,14 @@
 """
 Next Greater Element 
-Medium Accuracy: 48.92% Submissions: 18132 Points: 4
-Given an array arr[ ] of size N having distinct elements, the task is to find the next greater element for each element of the array in order of their appearance in the array.
-Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
-If there does not exist next greater of current element, then next greater element for current element is -1. For example, next greater of the last element is always -1.
+Medium 
+Given an array arr[ ] of size N having distinct elements, the task is to find 
+the next greater element for each element of the array in order of their 
+appearance in the array.
+Next greater element of an element in the array is the nearest element on the 
+right which is greater than the current element.
+If there does not exist next greater of current element, then next greater 
+element for current element is -1. For example, next greater of the last 
+element is always -1.
 
 Example 1:
 
