35f9bbf5d8b71ec6e519e044bb9e1d98675ed56d
Uday Kiran <kiranu941@gmail.com>
2021-07-29 21:59:23 +0530

    modelling

--- Algorithms And Techniques/Dynamic Programing/6-DP on Bitmask/1-People And Jobs.cpp
+++ Algorithms And Techniques/Dynamic Programing/6-DP on Bitmask/1-People And Jobs.cpp
@@ -2,20 +2,26 @@
 
 Problem Statement
 
-Let there be N workers and N jobs. Any worker can be assigned to perform any job, incurring some cost that may vary depending on the work-job assignment. It is required to perform all jobs by assigning exactly one worker to each job and exactly one job to each agent in such a way that the total cost of the assignment is minimized.
+Let there be N workers and N jobs. Any worker can be assigned to perform any job, 
+incurring some cost that may vary depending on the work-job assignment. 
+It is required to perform all jobs by assigning exactly one worker to 
+each job and exactly one job to each agent in such a way that the total 
+cost of the assignment is minimized.
 
 Input Format
 Number of workers and job: N
-Cost matrix C with dimension N*N where C(i,j) is the cost incurred on assigning ith Person to jth Job.
+Cost matrix C with dimension N*N where C(i,j) is the cost incurred on 
+assigning ith Person to jth Job.
 
 Sample Input
 4
 
 [
-9 2 7 8
-6 4 3 7
-5 8 1 8
-7 6 9 4
+
+People 1    9 2 7 8
+People 2    16 4 3 7
+People 3    5 8 1 8
+People 4    7 6 9 4
 ]
 
 Sample Output
@@ -25,6 +31,8 @@ Constraints
 N <= 20
 
 */
+
+
 
 #include <iostream>
 #include <bits/stdc++.h>
