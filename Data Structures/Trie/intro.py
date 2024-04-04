9b9db23b1eeb0c7e3bd141b30b5f5adc98db2a4f
Uday Kiran Padhy <kiranu941@gmail.com>
2024-04-04 21:05:09 +0530

    Revision of Trie Data Structure

--- Data Structures/Trie/intro.py
+++ Data Structures/Trie/intro.py
@@ -4,11 +4,11 @@ Let's jump into the topic.
 
 What is Trie?
 
-Trie is a tree-like data structure wherein all the nodes stores alphabetical values (most commonly).
-Trie is an ordered tree data structure in which every traversal down the branch retrieves you a string or word.
-Here is a formal definition from Wikipedia,
-A trie also called a digital tree or prefix tree, is a kind of search tree—an ordered tree data structure used to store a dynamic set or associative array where the keys are usually strings.
-Fun Fact: Trie name comes from name retrieval.
+Trie is a tree-like data structure wherein all the nodes stores alphabetical values (most commonly). Trie is an
+ordered tree data structure in which every traversal down the branch retrieves you a string or word. Here is a formal
+definition from Wikipedia, A trie also called a digital tree or prefix tree, is a kind of search tree—an ordered tree
+data structure used to store a dynamic set or associative array where the keys are usually strings. Fun Fact: Trie
+name comes from name retrieval.
 
 Representation:
 
@@ -140,3 +140,67 @@ If you still have a hard time understanding it. I attached some articles and you
 # https://www.youtube.com/watch?v=dOXfffhl4uI&list=PLfqMhTWNBTe0b2nM6JHVCnAkhQRGiZMSJ&index=189&ab_channel=ApnaCollege
 
 # https://stackoverflow.com/questions/11015320/how-to-create-a-trie-in-python/11015381#11015381
+
+
+class TrieNode:
+    def __init__(self):
+        # Dict: Key = letter, Item = TrieNode
+        self.children = {}
+        self.end = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def build_trie(self, words):
+        for word in words:
+            self.insert(word)
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.end = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                return False
+
+        return node.end
+
+    def _walk_trie(self, node, word, word_list):
+
+        if node.children:
+            for char in node.children:
+                word_new = word + char
+                if node.children[char].end:
+                    # if node.end:
+                    word_list.append(word_new)
+                    # word_list.append( word)
+                self._walk_trie(node.children[char], word_new, word_list)
+
+    def auto_complete(self, partial_word):
+        node = self.root
+
+        word_list = []
+        # find the node for last char of word
+        for char in partial_word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                # partial_word not found return
+                return word_list
+
+        if node.end:
+            word_list.append(partial_word)
+
+        #  word_list will be created in this method for suggestions that start with partial_word
+        self._walk_trie(node, partial_word, word_list)
+        return word_list
